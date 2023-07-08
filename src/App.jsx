477fa53bc9155f9347096295162febf6a1d447dd
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-08 11:08:44 +0200

    Feat: Basic estructure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import { Header } from './common/Header/Header'
+import { Body } from './views/00-Body/Body'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Body />
     </>
   )
 }
