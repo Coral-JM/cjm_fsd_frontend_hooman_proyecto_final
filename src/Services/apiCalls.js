41423df02eeeda9aa8a80ad8557efe4fb97b3973
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-30 17:11:35 +0200

    Feat: another /api to try the deploy

--- src/Services/apiCalls.js
+++ src/Services/apiCalls.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const root = "http://https://hooman-project-seven.vercel.app/api/api";
+const root = "http://https://hooman-project-seven.vercel.app/api";
 
 
 export const registerMe = async (user) => {
