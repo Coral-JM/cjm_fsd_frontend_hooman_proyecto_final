487b42eb2c57bab07de33932285cfd0befae7d5a
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-30 17:30:33 +0200

    Fix: route from useful updated

--- src/views/02-Register/Register.jsx
+++ src/views/02-Register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import './Register.css'
 import { useNavigate } from 'react-router-dom'
-import { checkError } from '../../services/useful'
+import { checkError } from '../../Services/useful'
 import { registerMe } from '../../Services/apiCalls'
 
 
