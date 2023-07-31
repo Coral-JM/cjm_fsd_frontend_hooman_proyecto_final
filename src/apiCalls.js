edd184d91896745fb09e4b825db2cfa0f26370eb
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-31 14:05:35 +0200

    Fix: api route changed for deploy in vercel

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const root = "https://hooman-backend.vercel.app/api";
+const root = "https://hooman-backend.vercel.app/api/api";
 
 
 export const registerMe = async (user) => {
