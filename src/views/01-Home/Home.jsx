3800c1af87a8ba7d2de6ba3c064828683e6451c4
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-31 14:24:31 +0200

    Fix: url image change to found in local

--- src/views/01-Home/Home.jsx
+++ src/views/01-Home/Home.jsx
@@ -25,7 +25,7 @@ export const Home = () => {
     <Container>
       <Row>
         <Col>
-          <div src="../../src/img/homeCover.png"
+          <div src="../src/img/homeCover.png"
             alt="Imagen de un galgo con un pañuelo y gafas"
             className="homePic"></div>
           <div className="banner">
