111e8633ca27babc911b8f5a533d63ac5e6c4740
Coral Jimenez <info.coraljimenez@gmail.com>
2023-08-01 20:39:23 +0200

    Fix: added bootstrap in get pending locals by admin

--- src/views/14-PendingLocals/PendingLocals.jsx
+++ src/views/14-PendingLocals/PendingLocals.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./PendingLocals.css";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { getLocalsByAdmin } from "../../apiCalls";
 import { useSelector } from "react-redux";
